022221fdf5bc9723020538971a76817ccbaaf57d
VengefulNick <89831583+VengefulNick@users.noreply.github.com>
2022-01-31 08:30:57 +0100

    more testing

--- witcher_test.py
+++ witcher_test.py
@@ -24,6 +24,12 @@ def main():
             del_char()
             
         elif menu_select == '5':
+            modify_char()
+            
+        elif menu_select == '6':
+            new_property()
+            
+        elif menu_select == '9':
             save_to_csv()
             
         elif menu_select == '0':
@@ -31,9 +37,12 @@ def main():
         else:
             print('Invalid option, try again!')
     
-""" Menu Functions """
- 
-def csv_to_json():
+"""
+Menu Functions
+"""
+
+
+def csv_to_json():  # 1
     print('Read CSV to Json\n')
 
                     # Objects in Array / Dicts in List
@@ -57,17 +66,15 @@ def csv_to_json():
         print('\tWrite Successful!\n')
 
 
-def show_json():
+def show_json():    # 2
     print('\nShow JSON Content\n')
     with open('witcher.json', 'r', encoding='utf-8') as jsonfile:
         read = json.load(jsonfile)
     
-    for i in read:
-        print(i)
-    print()
+    for row in read:
+        print(row)
 
-
-def add_char(new):
+def add_char(new):  # 3
     print('\nAdd new character!\n')
     with open('witcher.json', 'r', encoding='utf_8') as jsonfile:
         read = json.load(jsonfile)
@@ -83,7 +90,7 @@ def add_char(new):
         print(f'\nOverwrite Successful!\n{new} was added!')
 
 
-def del_char():
+def del_char(): # 4
     print('\nDelete character!\n')
     with open('witcher.json', 'r', encoding='utf-8') as jsonfile:
         data = json.load(jsonfile)
@@ -96,14 +103,74 @@ def del_char():
             data.pop(del_input)
             print(f'\n\tSuccessfully removed character on index: {del_input}')
         except IndexError:
-            print('Number is not in list, try again!')
+            print('Number is not in list, returning to menu!')
 
 
     with open('witcher.json', 'w', encoding='utf-8') as jsonfile:
         json.dump(data, jsonfile, indent=4)
 
 
-def save_to_csv():
+def modify_char():  # 5
+    print('\nModify character!\n')
+    with open('witcher.json', 'r', encoding='utf-8') as jsonfile:
+        data = json.load(jsonfile)
+        
+    for index, value in enumerate(data):
+        print(f'{index}: {value}')
+    print('\nEnter index of character to modify:')
+    mod_input = input_num()
+    try:
+        print(data[mod_input])
+        modmenu()
+        mod_select = input('Enter number from menu: ')
+        if mod_select == '1':
+            print('\nModify name')
+            new_name = input_str()
+            data[mod_input]['Name'] = new_name
+            print(f'Successfully modified character name to: {new_name}')
+        elif mod_select == '2':
+            print('\nModify alias')
+            new_alias = input_str()
+            data[mod_input]['Alias'] = new_alias
+            print(f'Successfully modified character name to: {new_alias}')
+        elif mod_select == '3':
+            print('\nModify proffession')
+            new_prof = input_str()
+            data[mod_input]['Profession'] = new_prof
+            print(f'Successfully modified character name to: {new_prof}')
+        elif mod_select == '4':
+            print('\nModify hair-color')
+            new_hair = input_str()
+            data[mod_input]['Hair-Color'] = new_hair
+            print(f'Successfully modified character name to: {new_hair}')
+        elif mod_select == '5':
+            print('\nModify eye-color')
+            new_eye = input_str()
+            data[mod_input]['Eye-Color'] = new_eye
+            print(f'Successfully modified character name to: {new_eye}') 
+        else:
+            print('Invalid choice. Returning to menu!\n')
+    except IndexError:
+        print('Number is not in list, returning to menu!\n')
+        
+    with open('witcher.json', 'w', encoding='utf-8') as jsonfile:
+        json.dump(data, jsonfile, indent=4)
+
+
+def new_property(): #6
+    with open('witcher.json', 'r', encoding='utf-8') as jsonfile:
+        data = json.load(jsonfile)
+    prop_name = input('Enter new property name: ')
+    count = 0
+    for row in data:
+        data[count][prop_name] = ""
+        print(f'Added property {prop_name} to {row}')
+        count += 1
+                
+    with open('witcher.json', 'w', encoding='utf-8') as jsonfile:
+        json.dump(data, jsonfile, indent=4)
+
+def save_to_csv(): #9
     print('\nSave json to CSV\n')
     with open('witcher.json', 'r', encoding='utf-8') as jsonfile:
         json_data = json.load(jsonfile)
@@ -119,21 +186,38 @@ def save_to_csv():
                 count += 1
             characters = character.values()
             csv_data.writerow(characters)
+    print('\n\tSave to CSV Successful!\n')
 
+"""
+Extra functions
+"""
 
-""" Extra functions """
 def mainmenu():
     print("""
-«----------------------»
- |   1. CSV to Json   |
- |   2. Show Json     |
- |   3. Add           |
- |   4. Delete        |
- |   5. Save to CSV   |
- |   0. Exit          |
-«----------------------»
+«-------Main-Menu-------»
+ |   1. CSV to Json    |
+ |   2. Show Json      |
+ |   3. Add Char       |
+ |   4. Delete Char    |
+ |   5. Modify Char    |
+ |   6. New Property   |
+ |   9. Save to CSV    |
+ |   0. Exit           |
+«-----------------------»
     """)
 
+
+def modmenu():
+    print('''
+    Which value do you want to modify:
+    1. Name
+    2. Alias
+    3. Proffession
+    4. Hair-Color
+    5. Eye-Color
+    ''')
+
+    
 def input_new():
     print('Enter name:')
     name_input = input_str()
@@ -169,6 +253,8 @@ def input_num():        # Inputchecker for integer.
         return input_num()
 
 
-""" Run Program """
+"""
+Run Program
+"""
 
 main()
